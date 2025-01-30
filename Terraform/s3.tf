80b90a97f6b4e46e0b73e84115aedb94624caef0
Nagacharan <89054489+charan-happy@users.noreply.github.com>
2025-01-30 17:55:09 +0000

    updated bucketname

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "deepseek_bucket" {
-  bucket = "deepseek-model-data"
+  bucket = "deepseek-model-data2"
   acl    = "private"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "deepseek_bucket_sse" {
-  bucket = aws_s3_bucket.deepseek_bucket.bucket
+  bucket = aws_s3_bucket.deepseek_bucket2.bucket
 
   rule {
     apply_server_side_encryption_by_default {
