f16d1262608f08e1feed1cdaefa8628e077e690d
Nagacharan <89054489+charan-happy@users.noreply.github.com>
2025-01-30 16:55:04 +0000

    Modified outputs.tf file

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,10 +2,11 @@ output "instance_public_ip" {
   value = aws_instance.deepseekmodel.public_ip
 }
 
-output "alb_lb.deepseek_alb.dns_name" {
+output "alb.dns_name" {
   value = aws_lb.deepseek_alb.dns_name
 }
 
 output "s3_bucket_name" {
   value = aws_s3_bucket.deepseek_bucket
 }
+
