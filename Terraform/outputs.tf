5a18aa06a527e37edebed7f2a8baef5b9ead5b39
Nagacharan <89054489+charan-happy@users.noreply.github.com>
2025-01-31 03:31:26 +0000

    updated output.tf file

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,12 +1,11 @@
-output "instance_public_ip" {
-  value = aws_instance.deepseekmodel.public_ip
-}
+output "instance_public_ip" {  
+  value = aws_instance.deepseekmodel.public_ip  
+}  
 
-output "alb_dns_name" {
-  value = aws_lb.deepseek_alb.dns_name
-}
+output "alb_dns_name" {  
+  value = aws_lb.deepseek_alb[0].dns_name  
+}  
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.deepseek_bucket
+output "s3_bucket_name" {  
+  value = aws_s3_bucket.deepseek_bucket.id  
 }
-
